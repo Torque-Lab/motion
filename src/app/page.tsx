96d71c9dcbb6afb7ff280840df5ccc10834f4628
Mathura <Torque-Lab@users.noreply.github.com>
2025-06-20 12:08:50 +0530

    useScroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,15 @@ import Dummy from "./Animation/TransForm";
 import Dummy2 from "./Animation/tranform2";
 import While from "./Animation/whileinview/while";
 import AnimatedBoxes from "./Animation/whileinview/while2";
+import Basic from "./Animation/useScroll/Basic";
+import ScrollAnimation from "./Animation/useScroll/example1";
 
 export default function Home() {
   return (
     <>
       Home
+      <ScrollAnimation />
+      <Basic />
       <AnimatedBoxes />
       <While />
       <Dummy2 />
